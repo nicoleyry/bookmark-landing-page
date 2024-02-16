07f604922179fa1b6c9b9714e92bac454ee7db9f
Nicole Yang <nicoleyry@outlook.com>
2024-02-16 14:11:36 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import Test from './components/Test';
+
+function App() {
+	return (
+		<div className='App'>
+			<Test />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Test.js
@@ -0,0 +1,10 @@
+import '../styles/test.scss';
+
+export default function Test() {
+
+	return(
+		<div className='test'>
+			<h1>Test</h1>
+		</div>
+	);
+};
